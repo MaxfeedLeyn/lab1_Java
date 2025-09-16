db41f0a8398f68e8965b4e67594820c911cdfafc
MaxfeedLeyn <maxnebelskiy@gmail.com>
2025-09-16 20:11:52 +0300

    preparation of the working environment

--- src/main/java/ua/university/Main.java
+++ src/main/java/ua/university/Main.java
@@ -1,10 +1,5 @@
 package ua.university;
 
-import ua.university.model.Person;
-
 public class Main {
-    public static void main(String[] args) {
-        Person person = new Person("Hjk", "Klkl", "jkj");
-        System.out.println(person);
-    }
+
 }
